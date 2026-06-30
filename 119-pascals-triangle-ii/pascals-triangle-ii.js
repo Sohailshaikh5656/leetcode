ffd3c2e6391b5406fefa7bed8bbf8f6187f6152c
Shaikh Sohel <134220674+Sohailshaikh5656@users.noreply.github.com>
2026-06-30 19:13:04 +0530

    Time: 0 ms (100.00%) | Memory: 51.9 MB (98.69%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    let n = rowIndex+1
+    if(n==0){
+        return []
+    }
+    if(n==1){
+        return [1]
+    }if(n==2){
+        return [1,1]
+    }
+
+    let pascal = [[1],[1,1]]
+    for(let i=2;i<n;i++){
+        let oldArr = pascal[i-1]
+        let newArr = [1]
+        for(let j=0;j<oldArr.length-1;j++){
+            newArr.push(oldArr[j]+oldArr[j+1])
+        }
+
+        newArr.push(1)
+        pascal.push(newArr)
+    }
+    return pascal[pascal.length-1]
+};
